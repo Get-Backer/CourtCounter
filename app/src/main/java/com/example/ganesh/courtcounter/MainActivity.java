7d874c3707d5181dc0aa4cb1e884934daec2cf12
Ganesh <ganeshpadwekar2989@gmail.com>
2016-09-17 12:50:48 -0400

    Add comment to MainActivity

--- app/src/main/java/com/example/ganesh/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/ganesh/courtcounter/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-
+//check comments
 public class MainActivity extends AppCompatActivity {
 
     int countA = 0;
